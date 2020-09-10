04ad5516b8d4393f8289e9737f20f8783cedb652
Usama26 <usama.waseem96@gmail.com>
2020-09-10 15:40:47 +0500

    Assignment 1 Question 1

---
+++ Assignment 1/q1.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 10 14:42:54 2020
+
+@author: Usama
+"""
+
+from scipy import spatial
+data = [
+        ("https://www.linkedin.com/in/ahmed-feroz-334409160/","Ahmed Feroz","Freelance Game Developer at Centrick Studios","Centrick Studios"),
+        ("https://www.linkedin.com/in/hassan-qureshi/","Hassan Qureshi","Python Engineer at Bitrupt","Bitrupt"),
+       ("https://www.linkedin.com/in/shammad/" , "Syed Hammad Ahmed" ,
+        "Fulbright PhD Scholar at University of Central Florida","University of Central Florida")]
+
+
+
+
+# studentName = input("Enter Student Name : ")
+# studentInterest = input("Enter Student Interest : ")
+
+studentName = "Usama"
+studentInterest = "I love coding in Python"
+
+print(studentInterest.split())
+
