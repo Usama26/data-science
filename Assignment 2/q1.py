f06d84bb17db04c9063032b488a0c7d811d1b13c
Usama26 <usama.waseem96@gmail.com>
2020-09-23 16:57:34 +0500

    Second File Outlier detection complete

--- Assignment 2/q1.py
+++ Assignment 2/q1.py
@@ -10,3 +10,35 @@ import pandas as pd
 first_health_att = pd.read_csv('./Data/First_Health_Camp_Attended.csv');
 second_health_att = pd.read_csv('./Data/Second_Health_Camp_Attended.csv');
 third_health_att = pd.read_csv('./Data/Third_Health_Camp_Attended.csv');
+
+
+
+first_health_att.isnull().sum()
+first_health_att.Health_Score
+
+second_health_att['Health_Camp_ID'].describe()
+quartiles = list(second_health_att['Health_Camp_ID'].quantile([0.25,0.5,0.75]))
+
+
+
+
+
+len(second_health_att['Health_Camp_ID'])/2
+
+print(3910/2)
+second_health_att['Health_Camp_ID'][1955]
+second_health_att['Health_Camp_ID'][1954]
+
+
+
+q1 = quartiles[0]
+q3 = quartiles[2]
+
+iqr = q3 - q1
+
+minRange = q1 - (1.5 * iqr)
+maxRange = q3 + (1.5 * iqr)
+
+for each in list(second_health_att.values):
+    if(each[1] < minRange or each[1] > maxRange):
+        print(each[1])
