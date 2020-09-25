bcde384e81007f48dae35fd7603010e78e2938e0
Usama26 <usama.waseem96@gmail.com>
2020-09-25 16:52:50 +0500

    Outliers Fixed

--- Assignment 2/q1.py
+++ Assignment 2/q1.py
@@ -14,7 +14,7 @@ third_health_att = pd.read_csv('./Data/Third_Health_Camp_Attended.csv');
 
 
 first_health_att.isnull().sum()
-first_health_att.Health_Score
+first_health_att.dropna("Health_Score",axis=1)
 
 second_health_att['Health_Camp_ID'].describe()
 quartiles = list(second_health_att['Health_Camp_ID'].quantile([0.25,0.5,0.75]))
@@ -36,9 +36,13 @@ q3 = quartiles[2]
 
 iqr = q3 - q1
 
-minRange = q1 - (1.5 * iqr)
-maxRange = q3 + (1.5 * iqr)
+minRange = q1 - (3.0* iqr)
+maxRange = q3 + (3.0* iqr)
 
-for each in list(second_health_att.values):
-    if(each[1] < minRange or each[1] > maxRange):
-        print(each[1])
+
+#Converting Outliers as the question asked
+for i in range(0,len(second_health_att)):
+    if(second_health_att['Health_Camp_ID'][i] < minRange or second_health_att['Health_Camp_ID'][i] > maxRange):
+        print(second_health_att['Health_Camp_ID'][i] , "X" ,i)
+        if(second_health_att['Health_Camp_ID'][i+1] - second_health_att['Health_Camp_ID'][i-1] <= 2):
+            second_health_att['Health_Camp_ID'][i] = second_health_att['Health_Camp_ID'][i+1]
